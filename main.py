f858353991572ae0cb09e521c02edfa043f797aa
My Personal Thinkpad <vangiang260694@gmail.com>
2023-04-07 15:32:45 +0700

    Done AccessWebsite Class

---
+++ main.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+
+class Whatsapp:
+    @staticmethod
+    def access_whatsapp():
+        driver_path = r"C:\Users\Admin\Desktop\Personal Documents" \
+                      r"Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
+                      r"Chrome_Driver\chromedriver.exe"
+        chrome_directory = r"user-data-dir=C:\Users\Admin\AppData" \
+                           r"\Local\Google\Chrome\User_Data_For_Auto_Trigger_System"
+        chrome_options = Options()
+        chrome_options.add_argument(chrome_directory)
+        chrome_options.add_experimental_option("detach", True)
+        service = Service(driver_path)
+        chrome_driver = webdriver.Chrome(service=service, options=chrome_options)
+        chrome_driver.set_page_load_timeout(3)
+        chrome_driver.get("https://web.whatsapp.com/")
+
+
+class AccessWebsite:
+    def __init__(self, url, timeout):
+        chrome_directory = r"user-data-dir=C:\Users\Admin\AppData" \
+                           r"\Local\Google\Chrome\User_Data_For_Auto_Trigger_System"
+        # Create a folder include data for every instance of chrome driver
+        driver_path = r"C:\Users\Admin\Desktop\Personal Documents" \
+                      r"Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
+                      r"Chrome_Driver\chromedriver.exe"
+        self.chrome_options = Options()
+        self.chrome_options.add_argument(chrome_directory)
+        self.chrome_options.add_experimental_option("detach", True)
+        # Keep the chrome driver open after the program is finished
+        self.service = Service(driver_path)
+        self.chrome_driver = webdriver.Chrome(service=self.service, options=self.chrome_options)
+        self.chrome_driver.set_page_load_timeout(self.timeout)
+        self.chrome_driver.get(url)
+        try:
+            self.chrome_driver.get(url)
+        except:
+            print("The website Is Not Available Or The Internet Connection Is Not Stable")
+        self.chrome_driver.get(url)
+        page_title = self.chrome_driver.title
+        # Get the title of the website
+        print(f"The Website You Are Accessing Is {page_title}")
+
+
+if __name__ == '__main__':
+    access_website = AccessWebsite("https://web.whatsapp.com/", 10)
