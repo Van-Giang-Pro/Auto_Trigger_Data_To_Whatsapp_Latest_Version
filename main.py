72afcfc78b95a47628b393213e16c66a76d93bc4
Business Laptop <vangiang260694@gmail.com>
2023-04-09 05:52:23 +0700

    Add Directory Link Of Driver Path And Chrome Folder For Business Laptop

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ from selenium.webdriver.chrome.service import Service
 class Whatsapp:
     @staticmethod
     def access_whatsapp():
-        driver_path = r"C:\Users\Admin\Desktop\Personal Documents" \
-                      r"Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
-                      r"Chrome_Driver\chromedriver.exe"
-        chrome_directory = r"user-data-dir=C:\Users\Admin\AppData" \
-                           r"\Local\Google\Chrome\User_Data_For_Auto_Trigger_System"
+        # driver_path = r"C:\Users\Admin\Desktop\Personal Documents" \
+        #               r"Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
+        #               r"Chrome_Driver\chromedriver.exe"
+        driver_path = r"C:\Users\fs120806\PycharmProjects" \
+                      r"Auto_Trigger_Data_To_Whatsapp_Latest_Version\Chrome_Driver" \
+                      r"\chromedriver.exe"
+        # chrome_directory = r"user-data-dir=C:\Users\Admin\AppData" \
+        #                    r"\Local\Google\Chrome\User_Data_For_Auto_Trigger_System"
+        chrome_directory = r"user-data-dir=C:\Users\fs120806\AppData\Local\Google\Chrome" \
+                           r"\User_Data_For_Auto_Trigger_System"
         chrome_options = Options()
         chrome_options.add_argument(chrome_directory)
         chrome_options.add_experimental_option("detach", True)
@@ -21,23 +26,30 @@ class Whatsapp:
 
 
 class AccessWebsite:
-    def __init__(self, url, timeout):
-        chrome_directory = r"user-data-dir=C:\Users\Admin\AppData" \
-                           r"\Local\Google\Chrome\User_Data_For_Auto_Trigger_System"
+    def __init__(self, url, web_page_timeout):
+        self.url = url
+        self.web_page_timeout = web_page_timeout
+        # chrome_directory = r"user-data-dir=C:\Users\Admin\AppData" \
+        #                    r"\Local\Google\Chrome\User_Data_For_Auto_Trigger_System"
+        chrome_directory = r"user-data-dir=C:\Users\fs120806\AppData\Local\Google\Chrome" \
+                           r"User_Data_For_Auto_Trigger_System"
         # Create a folder include data for every instance of chrome driver
-        driver_path = r"C:\Users\Admin\Desktop\Personal Documents" \
-                      r"Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
-                      r"Chrome_Driver\chromedriver.exe"
+        # driver_path = r"C:\Users\Admin\Desktop\Personal Documents" \
+        #               r"Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
+        #               r"Chrome_Driver\chromedriver.exe"
+        driver_path = r"C:\Users\fs120806\PycharmProjects" \
+                      r"Auto_Trigger_Data_To_Whatsapp_Latest_Version\Chrome_Driver" \
+                      r"\chromedriver.exe"
         self.chrome_options = Options()
         self.chrome_options.add_argument(chrome_directory)
         self.chrome_options.add_experimental_option("detach", True)
         # Keep the chrome driver open after the program is finished
         self.service = Service(driver_path)
         self.chrome_driver = webdriver.Chrome(service=self.service, options=self.chrome_options)
-        self.chrome_driver.set_page_load_timeout(self.timeout)
-        self.chrome_driver.get(url)
+        self.chrome_driver.set_page_load_timeout(self.web_page_timeout)
+        self.chrome_driver.get(self.url)
         try:
-            self.chrome_driver.get(url)
+            self.chrome_driver.get(self.url)
         except:
             print("The website Is Not Available Or The Internet Connection Is Not Stable")
         self.chrome_driver.get(url)
@@ -48,3 +60,5 @@ class AccessWebsite:
 
 if __name__ == '__main__':
     access_website = AccessWebsite("https://web.whatsapp.com/", 10)
+
+
