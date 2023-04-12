e36ab7de16a544dede6d24fcf32e8d1835ef7250
My Personal Thinkpad <vangiang260694@gmail.com>
2023-04-12 15:31:57 +0700

    Add Trigger Information Class And File

--- main.py
+++ main.py
@@ -3,19 +3,29 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 
+class TriggerInformation:
+    def __init__(self, trigger_information_path):
+        self.trigger_information_path = trigger_information_path
+    def read_trigger_information(self):
+        with open(self.trigger_information_path, 'r') as file:
+            csvreader = csv.reader(file)
+            for row in csvreader:
+                print(row)
+
+
 class Whatsapp:
     @staticmethod
     def access_whatsapp():
-        # driver_path = r"C:\Users\Admin\Desktop\Personal Documents" \
-        #               r"Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
-        #               r"Chrome_Driver\chromedriver.exe"
-        driver_path = r"C:\Users\fs120806\PycharmProjects" \
-                      r"Auto_Trigger_Data_To_Whatsapp_Latest_Version\Chrome_Driver" \
-                      r"\chromedriver.exe"
-        # chrome_directory = r"user-data-dir=C:\Users\Admin\AppData" \
-        #                    r"\Local\Google\Chrome\User_Data_For_Auto_Trigger_System"
-        chrome_directory = r"user-data-dir=C:\Users\fs120806\AppData\Local\Google\Chrome" \
-                           r"\User_Data_For_Auto_Trigger_System"
+        driver_path = r"C:\Users\Admin\Desktop\Personal Documents" \
+                      r"Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
+                      r"Chrome_Driver\chromedriver.exe"
+        # driver_path = r"C:\Users\fs120806\PycharmProjects" \
+        #               r"Auto_Trigger_Data_To_Whatsapp_Latest_Version\Chrome_Driver" \
+        #               r"\chromedriver.exe"
+        chrome_directory = r"user-data-dir=C:\Users\Admin\AppData" \
+                           r"\Local\Google\Chrome\User_Data_For_Auto_Trigger_System"
+        # chrome_directory = r"user-data-dir=C:\Users\fs120806\AppData\Local\Google\Chrome" \
+        #                    r"\User_Data_For_Auto_Trigger_System"
         chrome_options = Options()
         chrome_options.add_argument(chrome_directory)
         chrome_options.add_experimental_option("detach", True)
@@ -29,17 +39,17 @@ class AccessWebsite:
     def __init__(self, url, web_page_timeout):
         self.url = url
         self.web_page_timeout = web_page_timeout
-        # chrome_directory = r"user-data-dir=C:\Users\Admin\AppData" \
-        #                    r"\Local\Google\Chrome\User_Data_For_Auto_Trigger_System"
-        chrome_directory = r"user-data-dir=C:\Users\fs120806\AppData\Local\Google\Chrome" \
-                           r"User_Data_For_Auto_Trigger_System"
+        chrome_directory = r"user-data-dir=C:\Users\Admin\AppData" \
+                           r"\Local\Google\Chrome\User_Data_For_Auto_Trigger_System"
+        # chrome_directory = r"user-data-dir=C:\Users\fs120806\AppData\Local\Google\Chrome" \
+        #                    r"User_Data_For_Auto_Trigger_System"
         # Create a folder include data for every instance of chrome driver
-        # driver_path = r"C:\Users\Admin\Desktop\Personal Documents" \
-        #               r"Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
-        #               r"Chrome_Driver\chromedriver.exe"
-        driver_path = r"C:\Users\fs120806\PycharmProjects" \
-                      r"Auto_Trigger_Data_To_Whatsapp_Latest_Version\Chrome_Driver" \
-                      r"\chromedriver.exe"
+        driver_path = r"C:\Users\Admin\Desktop\Personal Documents" \
+                      r"Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
+                      r"Chrome_Driver\chromedriver.exe"
+        # driver_path = r"C:\Users\fs120806\PycharmProjects" \
+        #               r"Auto_Trigger_Data_To_Whatsapp_Latest_Version\Chrome_Driver" \
+        #               r"\chromedriver.exe"
         self.chrome_options = Options()
         self.chrome_options.add_argument(chrome_directory)
         self.chrome_options.add_experimental_option("detach", True)
