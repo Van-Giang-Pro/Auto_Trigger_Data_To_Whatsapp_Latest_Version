238c947c6e10bb228ce4a5365c9b18f285c0454c
My Personal Thinkpad <vangiang260694@gmail.com>
2023-04-21 04:32:14 +0700

    Get User Data From File

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+import pandas as pd
 
 
 class TriggerInformation:
@@ -8,17 +9,16 @@ class TriggerInformation:
         self.trigger_information_path = trigger_information_path
     def read_trigger_information(self):
         with open(self.trigger_information_path, 'r') as file:
-            csvreader = csv.reader(file)
-            for row in csvreader:
-                print(row)
+            self.df = pd.read_csv(file)
+        return self.df
 
 
 class Whatsapp:
     @staticmethod
     def access_whatsapp():
         driver_path = r"C:\Users\Admin\Desktop\Personal Documents" \
-                      r"Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
-                      r"Chrome_Driver\chromedriver.exe"
+                      r"\Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
+                      r"\Chrome_Driver\chromedriver.exe"
         # driver_path = r"C:\Users\fs120806\PycharmProjects" \
         #               r"Auto_Trigger_Data_To_Whatsapp_Latest_Version\Chrome_Driver" \
         #               r"\chromedriver.exe"
@@ -35,7 +35,7 @@ class Whatsapp:
         chrome_driver.get("https://web.whatsapp.com/")
 
 
-class AccessWebsite:
+class AccessWebsiteConfig(TriggerInformation):
     def __init__(self, url, web_page_timeout):
         self.url = url
         self.web_page_timeout = web_page_timeout
@@ -45,10 +45,10 @@ class AccessWebsite:
         #                    r"User_Data_For_Auto_Trigger_System"
         # Create a folder include data for every instance of chrome driver
         driver_path = r"C:\Users\Admin\Desktop\Personal Documents" \
-                      r"Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
-                      r"Chrome_Driver\chromedriver.exe"
+                      r"\Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
+                      r"\Chrome_Driver\chromedriver.exe"
         # driver_path = r"C:\Users\fs120806\PycharmProjects" \
-        #               r"Auto_Trigger_Data_To_Whatsapp_Latest_Version\Chrome_Driver" \
+        #               r"\Auto_Trigger_Data_To_Whatsapp_Latest_Version\Chrome_Driver" \
         #               r"\chromedriver.exe"
         self.chrome_options = Options()
         self.chrome_options.add_argument(chrome_directory)
@@ -57,18 +57,28 @@ class AccessWebsite:
         self.service = Service(driver_path)
         self.chrome_driver = webdriver.Chrome(service=self.service, options=self.chrome_options)
         self.chrome_driver.set_page_load_timeout(self.web_page_timeout)
-        self.chrome_driver.get(self.url)
         try:
             self.chrome_driver.get(self.url)
         except:
             print("The website Is Not Available Or The Internet Connection Is Not Stable")
-        self.chrome_driver.get(url)
         page_title = self.chrome_driver.title
         # Get the title of the website
         print(f"The Website You Are Accessing Is {page_title}")
+    def proceed_trigger(self):
+        user = AccessWebsiteConfig.read_trigger_information()
+        print(user)
 
 
 if __name__ == '__main__':
-    access_website = AccessWebsite("https://web.whatsapp.com/", 10)
+    access_website = AccessWebsiteConfig("https://web.whatsapp.com/", 1)
+    triggerinformation= TriggerInformation(r"C:\Users\Admin\Desktop\Personal Documents\Python Project" \
+                                            r"\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
+                                            r"\Trigger_Information\data_trigger.csv")
+    access_website.proceed_trigger()
+
+
+
+
+
 
 
