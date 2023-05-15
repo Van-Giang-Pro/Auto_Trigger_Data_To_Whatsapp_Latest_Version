58d303ddc36ec5cbd086d4973ae1959fd5862c3e
My Personal Thinkpad <vangiang260694@gmail.com>
2023-05-15 14:50:22 +0700

    Processing To Send Image

--- main.py
+++ main.py
@@ -11,19 +11,20 @@ import time
 
 
 class Whatsapp:
+
     def __init__(self, url, trigger_path):
         self.url = url
         self.trigger_path = trigger_path
 
         # Set Up For Personal Laptop
-        # driver_path = r"C:\Users\Admin\Desktop\Personal Documents" \
-        #               r"\Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
-        #               r"\Chrome_Driver\chromedriver.exe"
+        driver_path = r"C:\Users\Admin\Desktop\Personal Documents" \
+                      r"\Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
+                      r"\Chrome_Driver\chromedriver.exe"
 
         # Set Up For Company PC
-        driver_path = r"C:\Users\fs120806\PycharmProjects" \
-                      r"Auto_Trigger_Data_To_Whatsapp_Latest_Version\Chrome_Driver" \
-                      r"\chromedriver.exe"
+        # driver_path = r"C:\Users\fs120806\PycharmProjects" \
+        #               r"Auto_Trigger_Data_To_Whatsapp_Latest_Version\Chrome_Driver" \
+        #               r"\chromedriver.exe"
 
         # Set Up For Home PC
         # driver_path = r"C:\Users\admin\PycharmProjects" \
@@ -31,12 +32,12 @@ class Whatsapp:
         #               r"\Chrome_Driver\chromedriver.exe"
 
         # Set Up For Personal Laptop
-        # chrome_directory = r"user-data-dir=C:\Users\Admin\AppData" \
-        #                    r"\Local\Google\Chrome\User_Data_For_Auto_Trigger_System"
+        chrome_directory = r"user-data-dir=C:\Users\Admin\AppData" \
+                           r"\Local\Google\Chrome\User_Data_For_Auto_Trigger_System"
 
         # Set Up For Company PC
-        chrome_directory = r"user-data-dir=C:\Users\fs120806\AppData\Local\Google\Chrome" \
-                           r"\User_Data_For_Auto_Trigger_System"
+        # chrome_directory = r"user-data-dir=C:\Users\fs120806\AppData\Local\Google\Chrome" \
+        #                    r"\User_Data_For_Auto_Trigger_System"
 
         # Set Up For Home PC
         # chrome_directory = r"user-data-dir=C:\Users\admin\AppData\Local\Google" \
@@ -64,48 +65,75 @@ class Whatsapp:
 		    user_dict_list.append(user)
 	    return user_dict_list
 
+    def run_script_export_image(self):
+	    user_dict_list = self.read_trigger_information()
+	    for user in user_dict_list:
+		    script_link = user['Content']
+		    print(script_link)
+		    period = user['Period']
+		    print(period)
+
     def send_message(self):
 	    wait = WebDriverWait(self.chrome_driver, 20)
-	    search_box_xpath = '//*[@id="side"]/div[1]/div/div/div[2]/div/div[1]/p'
-	    message_box_xpath = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p'
 	    try:
 	        search_box = wait.until(EC.presence_of_element_located((By.XPATH, search_box_xpath)))
 	    except TimeoutException:
-	        print("The Website Is Not Available Or The Internet Connection Is Not Stable")
-	        self.chrome_driver.quit()
-	        return None
-	        for user in user_dict_list:
-				search_box.clear()
-				search_box.click()
-				search_box.send_keys(user['User'])
-				time.sleep(1)
-				search_box.send_keys(Keys.ENTER)
-				print("Start Sending Message To {0}".format(user['User']))
-				time.sleep(1)
-				message_box = wait.until(EC.presence_of_element_located((By.XPATH, message_box_xpath)))
-		        message_box.clear()
-		        message_box.click()
-		        message_box.send_keys(user['Text'])
-		        time.sleep(1)
-		        message_box.send_keys(Keys.ENTER)
-		        time.sleep(3)
+		    print("The Website Is Not Available Or The Internet Connection Is Not Stable")
+		    self.chrome_driver.quit()
+		    return None
+	    for user in user_dict_list:
+		    search_box.clear()
+		    search_box.click()
+		    search_box.send_keys(user['User'])
+		    search_box.send_keys(Keys.ENTER)
+		    print("Start Sending Message To {0}".format(user['User']))
+		    message_box = wait.until(EC.presence_of_element_located((By.XPATH, message_box_xpath)))
+		    message_box.clear()
+		    message_box.click()
+		    message_box.send_keys(user['Text'])
+		    time.sleep(1) # Wait for the message to be sent
+		    message_box.send_keys(Keys.ENTER)
+
+    def send_image(self, image_path_folder):
+	    for user in user_dict_list:
+		    search_box.clear()
+		    search_box.click()
+		    search_box.send_keys(user['User'])
+		    search_box.send_keys(Keys.ENTER)
+		    print("Start Sending Message To {0}".format(user['User']))
+		    message_box = wait.until(EC.presence_of_element_located((By.XPATH, message_box_xpath)))
+		    message_box.clear()
+		    message_box.click()
+		    message_box.send_keys(user['Text'])
+		    time.sleep(1)  # Wait for the message to be sent
+		    message_box.send_keys(Keys.ENTER)
+		    print("Start Sending Image To {0}".format(user['User']))
+		    attachment_icon = wait.until(EC.presence_of_element_located((By.XPATH, attachment_icon_xpath)))
+		    attachment_icon.click()
+		    image_attachment = wait.until(EC.presence_of_element_located((By.XPATH, image_attachment_xpath)))
+		    image_attachment.send_keys(image_path_folder)
+		    time.sleep(1)  # Wait for the image to be uploaded
+		    send_image_button = wait.until(EC.presence_of_element_located((By.XPATH, send_button_xpath)))
+		    send_image_button.click()
 
 
 if __name__ == '__main__':
 	# Set Up For Business PC
-	trigger_path = (r"C:\Users\fs120806\PycharmProjects" \
-	                r"\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
-	                r"\Trigger_Information\data_trigger.csv")
-	# Set Up For Personal Laptop
-	# trigger_path = (r"C:\Users\Admin\Desktop\Personal Documents\Python Project" \
+	# trigger_path = (r"C:\Users\fs120806\PycharmProjects" \
 	#                 r"\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
 	#                 r"\Trigger_Information\data_trigger.csv")
+	# Set Up For Personal Laptop
+	trigger_path = r"C:\Users\Admin\Desktop\Personal Documents\Python Project" \
+	                r"\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
+	                r"\Trigger_Information\data_trigger.csv"
+	image_path_folder = r"C:\Users\Admin\Desktop\Personal Documents\Python Project" \
+						 "Auto_Trigger_Data_To_Whatsapp_Latest_Version\Image\Giang.png"
 	url = "https://web.whatsapp.com/"
 	whatsapp = Whatsapp(url, trigger_path)
 	user_dict_list = whatsapp.read_trigger_information()
-	whatsapp.send_message
-	print(user_dict_list)
-	print(len(user_dict_list))
+	whatsapp.send_message()
+	whatsapp.send_image(image_path_folder=image_path_folder)
+	whatsapp.run_script_export_image()
 
 
 
