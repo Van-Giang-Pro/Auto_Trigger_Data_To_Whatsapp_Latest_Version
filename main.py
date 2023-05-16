1b1c3643e3c3a950bffa1e6d16ee7fc1cb72c1a4
Business Laptop <vangiang260694@gmail.com>
2023-05-17 05:35:26 +0700

    Modify Whatsapp Class For Sending Image And Message

--- main.py
+++ main.py
@@ -11,20 +11,25 @@ import time
 
 
 class Whatsapp:
+    search_box_xpath = "//div[@title='Tìm kiếm bằng ô nhập văn bản']"
+    message_box_xpath = "//div[@title='Nhập tin nhắn']"
+    attachment_icon_xpath = ''
+    image_attachment_xpath = ''
+    send_button_xpath = ''
 
     def __init__(self, url, trigger_path):
         self.url = url
         self.trigger_path = trigger_path
 
         # Set Up For Personal Laptop
-        driver_path = r"C:\Users\Admin\Desktop\Personal Documents" \
-                      r"\Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
-                      r"\Chrome_Driver\chromedriver.exe"
+        # driver_path = r"C:\Users\Admin\Desktop\Personal Documents" \
+        #               r"\Python Project\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
+        #               r"\Chrome_Driver\chromedriver.exe"
 
-        # Set Up For Company PC
-        # driver_path = r"C:\Users\fs120806\PycharmProjects" \
-        #               r"Auto_Trigger_Data_To_Whatsapp_Latest_Version\Chrome_Driver" \
-        #               r"\chromedriver.exe"
+        # Set Up For Company Laptop
+        driver_path = r"C:\Users\fs120806\PycharmProjects" \
+                      r"Auto_Trigger_Data_To_Whatsapp_Latest_Version\Chrome_Driver" \
+                      r"\chromedriver.exe"
 
         # Set Up For Home PC
         # driver_path = r"C:\Users\admin\PycharmProjects" \
@@ -32,12 +37,12 @@ class Whatsapp:
         #               r"\Chrome_Driver\chromedriver.exe"
 
         # Set Up For Personal Laptop
-        chrome_directory = r"user-data-dir=C:\Users\Admin\AppData" \
-                           r"\Local\Google\Chrome\User_Data_For_Auto_Trigger_System"
+        # chrome_directory = r"user-data-dir=C:\Users\Admin\AppData" \
+        #                    r"\Local\Google\Chrome\User_Data_For_Auto_Trigger_System"
 
-        # Set Up For Company PC
-        # chrome_directory = r"user-data-dir=C:\Users\fs120806\AppData\Local\Google\Chrome" \
-        #                    r"\User_Data_For_Auto_Trigger_System"
+        # Set Up For Company Laptop
+        chrome_directory = r"user-data-dir=C:\Users\fs120806\AppData\Local\Google\Chrome" \
+                           r"\User_Data_For_Auto_Trigger_System"
 
         # Set Up For Home PC
         # chrome_directory = r"user-data-dir=C:\Users\admin\AppData\Local\Google" \
@@ -74,9 +79,9 @@ class Whatsapp:
 		    print(period)
 
     def send_message(self):
-	    wait = WebDriverWait(self.chrome_driver, 20)
+	    wait = WebDriverWait(self.chrome_driver, 40)
 	    try:
-	        search_box = wait.until(EC.presence_of_element_located((By.XPATH, search_box_xpath)))
+	        search_box = wait.until(EC.presence_of_element_located((By.XPATH, Whatsapp.search_box_xpath)))
 	    except TimeoutException:
 		    print("The Website Is Not Available Or The Internet Connection Is Not Stable")
 		    self.chrome_driver.quit()
@@ -87,14 +92,21 @@ class Whatsapp:
 		    search_box.send_keys(user['User'])
 		    search_box.send_keys(Keys.ENTER)
 		    print("Start Sending Message To {0}".format(user['User']))
-		    message_box = wait.until(EC.presence_of_element_located((By.XPATH, message_box_xpath)))
+		    message_box = wait.until(EC.presence_of_element_located((By.XPATH, Whatsapp.message_box_xpath)))
 		    message_box.clear()
 		    message_box.click()
 		    message_box.send_keys(user['Text'])
 		    time.sleep(1) # Wait for the message to be sent
 		    message_box.send_keys(Keys.ENTER)
 
+
     def send_image(self, image_path_folder):
+	    wait = WebDriverWait(self.chrome_driver, 40)
+	    search_box_xpath = ''
+	    message_box_xpath = ''
+	    attachment_icon_xpath = ''
+	    image_attachment_xpath = ''
+	    send_button_xpath = ''
 	    for user in user_dict_list:
 		    search_box.clear()
 		    search_box.click()
@@ -118,21 +130,27 @@ class Whatsapp:
 
 
 if __name__ == '__main__':
-	# Set Up For Business PC
-	# trigger_path = (r"C:\Users\fs120806\PycharmProjects" \
-	#                 r"\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
-	#                 r"\Trigger_Information\data_trigger.csv")
-	# Set Up For Personal Laptop
-	trigger_path = r"C:\Users\Admin\Desktop\Personal Documents\Python Project" \
+	# Set Up For Company Laptop
+	trigger_path = (r"C:\Users\fs120806\PycharmProjects" \
 	                r"\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
-	                r"\Trigger_Information\data_trigger.csv"
-	image_path_folder = r"C:\Users\Admin\Desktop\Personal Documents\Python Project" \
-						 "Auto_Trigger_Data_To_Whatsapp_Latest_Version\Image\Giang.png"
+	                r"\Trigger_Information\data_trigger.csv")
+	# Set Up For Personal Laptop
+	# trigger_path = r"C:\Users\Admin\Desktop\Personal Documents\Python Project" \
+	#                 r"\Auto_Trigger_Data_To_Whatsapp_Latest_Version" \
+	#                 r"\Trigger_Information\data_trigger.csv"
+
+	# Set Up For Personal Laptop
+	# image_path_folder = r"C:\Users\Admin\Desktop\Personal Documents\Python Project" \
+	# 					 "\Auto_Trigger_Data_To_Whatsapp_Latest_Version\Image\Giang.png"
+
+	# Set Up For Company Laptop
+	image_path_folder = (r"C:\Users\fs120806\PycharmProjects" \
+	                    r"\Auto_Trigger_Data_To_Whatsapp_Latest_Version\Image\Giang.png")
 	url = "https://web.whatsapp.com/"
 	whatsapp = Whatsapp(url, trigger_path)
 	user_dict_list = whatsapp.read_trigger_information()
 	whatsapp.send_message()
-	whatsapp.send_image(image_path_folder=image_path_folder)
+	# whatsapp.send_image(image_path_folder=image_path_folder)
 	whatsapp.run_script_export_image()
 
 
